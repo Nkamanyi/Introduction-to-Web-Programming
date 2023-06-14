58b644ec59cc853a225b1435e17d32fe88279a3b
Nkamanyi <Chatutie.Nkamanyimartin@tuni.fi>
2023-06-15 01:10:19 +0300

    Deleted unwanted material

--- Week_1/js/basic.js
+++ Week_1/js/basic.js
@@ -33,46 +33,4 @@ if (document.readyState !== "loading") {
     });
   }
   
-  /*console.log("Hello World!");
-  
-  let numbers = [];
-  
-  numbers.push(8)
-  numbers.push(16)
-  numbers.push(256)
-  numbers.push(32)
-  
-  console.log(numbers);
-  
-  numbers.unshift(666)
-  
-  console.log(numbers);
-  
-  let number = numbers.pop()
-  
-  console.log(numbers);
-  console.log(number);
-  
-  let student = {
-    name: "David",
-    id: "0359875"
-  }
-  
-  let students = [];
-  
-  students.push(student)
-  students.push({name: "Chen", id: "0597856"})
-  students.push({name: "Jonna", id: "0597356"})
-  students.push({name: "Veeti", id: "0897856"})
-  
-  console.log(students);
-  
-  students.forEach(s => console.log(s))
-  
-  while (students.length > 0) {
-    console.log(students.pop())
-  }
-  console.log(students);
-  
-  */
   
